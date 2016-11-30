3e795662ab26a898671236549451ba0135a3712d
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-30 16:10:43 +0100

    Prelim Setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  include ActionController::ImplicitRender
+  respond_to :json
+
+  rescue_from RailsParam::Param::InvalidParameterError do |exception|
+    render json: {errors: exception}, status: 422
+  end
+
 end
