757afaeef645c535fee025447f8e705c4e5a95ea
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-30 16:14:10 +0100

    model & controller & routes User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def create
+    param! :uid, String, required: true
+    @user = User.create(uid: params[:uid])
+    render json: @user, status: 201
+  end
+
+  def destroy
+    param! :id, String, required: true
+    @user = User.find_by_uid(params[:id])
+    @user.destroy
+    head status: 204
+  end
+end
